39232436c746c2da98c5635a59d1ffa240c3522d
Stefano Consonni <stefanoconsonni@ymail.com>
2021-10-16 20:29:22 +0200

    completed blog form

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,21 @@ const Create = () => {
 	return (
 		<div className="create">
 			<h2>Add a New Blog</h2>
+			<form>
+				<label>Blog title:</label>
+				<input type="text" required />
+				<label>Blog body:</label>
+				<textarea required></textarea>
+				<label>Blog author:</label>
+				<select>
+					<option value="andrea">Andrea</option>
+					<option value="barbara">Barbara</option>
+					<option value="francesco">Francesco</option>
+					<option value="luca">Luca</option>
+					<option value="sabrina">Sabrina</option>
+				</select>
+				<button>Add Blog</button>
+			</form>
 		</div>
 	);
 };
